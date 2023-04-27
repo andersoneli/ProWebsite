d50f9fb0eeb4bb11d46cadd1e21d9fe226f28d16
Eli Anderson <eliaanderson@uiowa.edu>
2023-04-27 14:13:09 -0500

    Added Many Features
    Privacy page
    Cookie popup
    cookie page
    updated imges

--- js/custom.js
+++ js/custom.js
@@ -41,4 +41,30 @@
     
   })(window.jQuery);
 
+// Get the modal
+var modal = document.getElementById("cookieModal");
+
+// Get the close button
+var closeButton = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the close button, hide the modal
+closeButton.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the page is loaded, show the modal if the cookie is not set
+window.onload = function() {
+  if (document.cookie.indexOf("acceptedCookies=true") < 0) {
+    modal.style.display = "block";
+  }
+}
+
+// When the user clicks the accept button, set a cookie and hide the modal
+var acceptButton = document.getElementById("acceptCookies");
+acceptButton.onclick = function() {
+  document.cookie = "acceptedCookies=true; expires=Thu, 31 Dec 2099 23:59:59 GMT; path=/";
+  modal.style.display = "none";
+}
+
+
   
